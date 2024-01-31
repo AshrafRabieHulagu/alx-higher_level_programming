dc0177565d4c00cce491c8529a31ddfe04e0de84
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2024-01-31 01:54:30 -0800

    0x07. Python - Test-driven development

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,21 +4,21 @@
 
 def add_integer(a, b=98):
     """Adds two integers.
-   
     Args:
         a: the first integer.
         b: the second integer, default 98.
-
     Raises:
         TypeError: if a, b are not int, float.
     Returns:
         The sum of the two integers.
     """
+
     if type(a) not in (int, float):
-        raise TypeError("a must be an integer")
+        raise TypeError('a must be an integer')
     if type(b) not in (int, float):
-        raise TypeError("b must be an integer")
+        raise TypeError('b must be an integer')
     return int(a) + int(b)
+
 if __name__ == "__main__":
     import doctest
     doctest.testfile("tests/0-add_integer.txt")
