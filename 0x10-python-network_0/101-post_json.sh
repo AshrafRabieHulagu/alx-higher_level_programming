f95cb2adf8c97e059b93449e9cea1dbaac875281
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2024-04-27 08:32:33 -0700

    0x10. Python - Network #0

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
-# Sends a JSON POST request to a URL and displays the body of the response
-if [ -f "$2" ]; then
-    curl -s -X POST -H "Content-Type: application/json" -d @"$2" "$1"
+
+# Check if both arguments are provided
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <URL> <filename>"
+    exit 1
+fi
+
+# Read the filename and check if it exists
+filename="$2"
+if [ ! -f "$filename" ]; then
+    echo "Error: File '$filename' not found."
+    exit 1
+fi
+
+# Send the JSON data from the file as a POST request
+curl -s -X POST -H "Content-Type: application/json" -d "@$filename" "$1"
+
+# Check if the response contains valid JSON
+if [ $? -eq 0 ]; then
+    echo "Valid JSON"
 else
     echo "Not a valid JSON"
 fi
