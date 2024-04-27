6904376e30882f7dd77021b5511aed32dc350d63
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2024-04-27 02:15:19 -0700

    0x10. Python - Network #0

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Check if the user provided a URL argument
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# Send a request to the URL and store the response body in a temporary file
+response=$(curl -sI "$1")
+content_length=$(echo "$response" | grep -i "Content-Length" | awk '{print $2}')
+
+# Display the size of the response body in bytes
+echo "$content_length"
---
+++ 0x10-python-network_0/README.md
@@ -0,0 +1,2 @@
+the soultion of project 0x10. Python - Network #0
+Submitted by: Ashraf Rabie Hulagu
