c105d9e79d76906860a4d609fa5b3eae69995acd
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2024-04-27 05:15:08 -0700

    0x10. Python - Network #0

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,26 +6,20 @@ def find_peak(list_of_integers):
         list_of_integers (list): List of unsorted integers.
 
     Returns:
-        int or None: A peak element from the list or None if the list is empty.
+        int: A peak element from the list.
     """
-    if not list_of_integers:
-        return None
-
     left, right = 0, len(list_of_integers) - 1
-    while left <= right:
+
+    while left < right:
         mid = (left + right) // 2
-        if (mid == 0 or list_of_integers[mid] >= list_of_integers[mid - 1]) and \
-           (mid == len(list_of_integers) - 1 or list_of_integers[mid] >= list_of_integers[mid + 1]):
-            return list_of_integers[mid]
-        elif mid > 0 and list_of_integers[mid - 1] > list_of_integers[mid]:
-            right = mid - 1
-        else:
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
             left = mid + 1
+        else:
+            right = mid
+
+    return list_of_integers[left]
 
 # Test cases
 print(find_peak([1, 2, 4, 6, 3]))    # Output: 6
-print(find_peak([4, 2, 1, 2, 3, 1])) # Output: 3
+print(find_peak([4, 2, 1, 2, 3, 1])) # Output: 4
 print(find_peak([2, 2, 2]))          # Output: 2
-print(find_peak([]))                 # Output: None
-print(find_peak([-2, -4, 2, 1]))     # Output: 2
-print(find_peak([4, 2, 1, 2, 2, 2, 3, 1])) # Output: 4
