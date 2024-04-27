6433b2cff9016af97a239b2ee25a97fe7546d727
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2024-04-27 02:20:34 -0700

    0x10. Python - Network #0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+# This script takes a URL as an argument, sends a request to that URL using curl in silent mode,
+and displays the size of the response body in bytes
+curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}'
 # Check if the user provided a URL argument
 if [ $# -ne 1 ]; then
     echo "Usage: $0 <URL>"
