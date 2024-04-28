7444baf852190126c46929b82c272a4a1fe27e74
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2024-04-27 17:57:08 -0700

    0x10. Python - Network #0

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-# Makes a request to a URL that causes the server to respond with a message containing "You got me!"
-curl -s -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
+# Makes a request to 0.0.0.0:5000/catch_me to get the response "You got me!"
+
+# Send a PUT request with custom header to cause the server to respond with "You got me!"
+curl -sX PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
