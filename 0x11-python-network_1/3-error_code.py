911db1f1bd695dbbe13b96359714685cee48c18d
Ashraf Rabie <ashraf.rabie.hulagu@gmail.com>
2024-05-05 06:55:14 -0700

    0x11-python-network_1

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-"""
-Script that takes in a URL, sends a request to the URL, and displays the body of the response (decoded in utf-8).
-If the HTTP status code is greater than or equal to 400, prints: "Error code:" followed by the HTTP status code
-"""
-
+''' a python script that sends request to url and print body'''
 import urllib.request
 import urllib.error
 import sys
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     url = sys.argv[1]
     try:
         with urllib.request.urlopen(url) as response:
-            print(response.read().decode('utf-8'))
+            body = response.read()
+            print(body.decode('utf-8'))
     except urllib.error.HTTPError as e:
-        print("Error code: {}".format(e.code))
+        print('Error code: {}'.format(e.code))
