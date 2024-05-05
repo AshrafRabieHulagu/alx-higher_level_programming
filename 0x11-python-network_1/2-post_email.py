881598b95b7b182b77f73289c76ccf636c4c88cf
Ashraf Rabie <ashraf.rabie.hulagu@gmail.com>
2024-05-05 06:53:18 -0700

    0x11-python-network_1

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""Sends a POST request to the passed URL with the email as a parameter"""
-
-import urllib.request
+''' a python script that sends a post request to a url'''
 import urllib.parse
+import urllib.request
 import sys
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     url = sys.argv[1]
-    values = {'email': sys.argv[2]}
-    data = urllib.parse.urlencode(values)
-    data = data.encode('ascii')
-    req = urllib.request.Request(url, data)
-    with urllib.request.urlopen(req) as response:
-        print(response.read().decode("utf-8"))
+    e_address = sys.argv[2]
+    data = urllib.parse.urlencode({'email': e_address})
+    data = data.encode('utf-8')
+    with urllib.request.urlopen(url, data) as response:
+        body = response.read()
+        print(body.decode('utf-8'))
