f2c89c5e71f4bbc65d72fd9afee42f21be8553ac
Ashraf Rabie <ashraf.rabie.hulagu@gmail.com>
2024-05-05 07:07:45 -0700

    0x11-python-network_1

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-'''a script that send request and returns body of url'''
+"""Headers again, different module"""
 import requests
 import sys
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        q = ""
+    else:
+        q = sys.argv[1]
+    dic = {'q': q}
     url = 'http://0.0.0.0:5000/search_user'
-    query = sys.argv[1] if len(sys.argv) > 1 else ""
-    data = {'q': query}
-    r = requests.post(url, data=data)
+    r = requests.post(url, dic)
     try:
-        content = r.json()
-        if content:
-            print("[{}] {}".format(content['id'], content['name']))
+        rj = r.json()
+        if rj == {}:
+            print('No result')
         else:
-            print("No result")
-    except Exception:
-        print("Not a valid JSON")
+            print('[{}] {}'.format(rj['id'], rj['name']))
+    except:
+        print('Not a valid JSON')
