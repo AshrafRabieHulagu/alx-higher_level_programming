2621af246865bdba5df66ada2a5351a4d21b9279
Ashraf Rabie <ashraf.rabie.hulagu@gmail.com>
2024-05-05 07:09:42 -0700

    0x11-python-network_1

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-"""Headers again, different module"""
+""" Script that takes in a letter and sends
+    a POST request to url with the letter as
+    a parameter. """
 import requests
-import sys
+from sys import argv
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        q = ""
+    if len(argv) == 2:
+        q = argv[1]
     else:
-        q = sys.argv[1]
-    dic = {'q': q}
+        q = ''
     url = 'http://0.0.0.0:5000/search_user'
-    r = requests.post(url, dic)
+    req = requests.post(url, data={'q': q})
     try:
-        rj = r.json()
-        if rj == {}:
+        data = req.json()
+        if not data:
             print('No result')
         else:
-            print('[{}] {}'.format(rj['id'], rj['name']))
-    except:
+            print('[{}] {}'.format(data.get('id'), data.get('name')))
+    except Exception:
         print('Not a valid JSON')
